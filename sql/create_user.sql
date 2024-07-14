2ffb30dacfa048f2abfac08ed1d986e7b498121f
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2024-07-15 07:36:09 +0900

    テーブル構造の追加を実施

---
+++ sql/create_user.sql
@@ -0,0 +1,8 @@
+Delete From UserInfo;
+Insert into UserInfo (
+    user_id, 
+    email,
+    nick_name
+) VALUES 
+("0190b21f-cb10-792c-a9aa-b975da7a4223","yutaka.kajiwara.22@gmail.com","ゆたにぃ"),
+("0190b21f-cb10-712b-85fb-575f99891e25","","わたなべ");
--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,5 +1,28 @@
-CREATE TABLE CharacterInfo (
+CREATE TABLE if not exists CharacterInfo (
     id INTEGER PRIMARY KEY , 
     name TEXT NOT NULL,
     filePath TEXT NOT NULL
 );
+
+CREATE TABLE if not exists UserInfo (
+    user_id TEXT PRIMARY KEY,
+    email TEXT,
+    nick_name TEXT
+);
+CREATE TABLE if not exists PlayerMatches (
+    match_id TEXT PRIMARY KEY,
+    player1_id TEXT,
+    player2_id TEXT,
+    player1_character_id INT,
+    player2_character_id INT,
+    winner_id TEXT,
+    match_date DATETIME,
+    round1_winner TEXT,
+    round2_winner TEXT,
+    round3_winner TEXT
+);
+CREATE TABLE PlayMatchMemo (
+    memo_id TEXT PRIMARY KEY,
+    memo_owner_id TEXT,
+    memo TEXT
+)
