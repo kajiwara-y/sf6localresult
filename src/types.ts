df3381dad40e8fabb210b8d581843e218e7d2de3
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2024-07-20 15:16:20 +0900

    User情報とMatch情報の定義を登録

--- src/types.ts
+++ src/types.ts
@@ -10,3 +10,20 @@ export type UserInfo = {
   email: string;
   nick_name: string;
 };
+
+export type UserName = {
+  nick_name: string;
+  user_id: string;
+};
+
+export type MatchInfo = {
+  match_id: string;
+  player1_id: string;
+  player2_id: string;
+  player1_character_id: number;
+  player2_character_id: number;
+  winner_id: string;
+  round1_winner: string;
+  round2_winner: string;
+  round3_winner: string | null;
+};
