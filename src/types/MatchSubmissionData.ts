a34943e2a8f5c736ebcbe243ba3d249db51690a3
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2024-07-21 20:46:51 +0900

    相手情報をtypeに定義

--- src/types/MatchSubmissionData.ts
+++ src/types/MatchSubmissionData.ts
@@ -1,6 +1,7 @@
 export type MatchSubmissionData = {
     playerSide: string
     myId: string
+    opponentId: string
     player1CharacterId: string
     player2CharacterId: string
     round1: string
