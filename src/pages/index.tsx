c802ae0eec07db0abbeab03bc928b59b6377c745
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2024-07-23 14:53:27 +0900

    デモ用にGoogle認証通過後はダミーデータで動くようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,9 @@ import { getAuth } from '@hono/oidc-auth';
 export const indexPage = async (c: Context) => {
   const auth = await getAuth(c)
   const characterInfo = await getAllCharacters(c)
-  const userInfo = await getUserInfo(c, auth?.email)
+  let userInfo = await getUserInfo(c, auth?.email)
+  if(userInfo?.user_id === undefined)
+    userInfo = await getUserInfo(c,"dummy@example.com")
   const userNames = await getUserNames(c, userInfo?.user_id)
 
   return c.html(<Top characterArray={characterInfo} userInfo={userInfo} userNames={userNames} />)
