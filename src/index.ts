3610279b44e5a7c8249a2b494d6efddd480bf7f7
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2024-07-23 11:03:42 +0900

    ミドルウェアの指定の順番が正しくなかったので修正を実施

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,16 @@ import { getCharacterInfo } from './api/character'
 import { oidcAuthMiddleware, getAuth } from '@hono/oidc-auth';
 
 const app = new Hono()
-app.get('/', indexPage)
-app.post('/', matchSubmitPage)
-app.get('/api/character', getCharacterInfo)
 app.use('*', oidcAuthMiddleware())
 app.use('*', async (c, next) => {
   // Authorize user with email address
   const auth = await getAuth(c)
+  if (!auth?.email.endsWith('@gmail.com')) {
+    return c.text('Unauthorized', 401)
+  }
   await next()
 })
-app.get('*', async (c) => {
-  const response = await c.env.ASSETS.fetch(c.req.raw);
-  // clone the response to return a response with modifiable headers
-  const newResponse = new Response(response.body, response)
-  return newResponse
-});
+app.get('/', indexPage)
+app.post('/', matchSubmitPage)
+app.get('/api/character', getCharacterInfo)
 export default app
