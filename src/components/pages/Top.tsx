9b7127ecb789ffa6b4f01591f4f96a939be65f4b
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2024-08-02 21:04:27 +0900

    Textは未実装な旨を追加

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -277,7 +277,8 @@ export const Top = (props: {
                   name="notes"
                   rows="3"
                   class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                ></textarea>
+                  disabled
+                >まだ未実装です・・・</textarea>
               </div>
 
               <button
