a618b8a762afd3a83cf01c2989a3b9ad3362a744
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2024-09-03 11:02:51 +0900

    名称を修正

--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -45,7 +45,7 @@ export const Top = (props: {
                 対戦履歴
               </a>
               <a href="/win-rate" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                キャラクター別勝率
+                勝率
               </a>
               <a href="/character-matchcount" class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
                 キャラクター別対戦数
